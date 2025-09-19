8618001f04c6c93fe5b0bb5bb0cf175a5e286f69
Tyler Stober <stobertg@gmail.com>
2025-09-19 15:07:50 +0200

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5a2509edb6fe4da1"; }
+/******/ 		__webpack_require__.h = function() { return "23db1b604c1fe56b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/theme/parts/colors.tsx
+++ src/theme/parts/colors.tsx
@@ -1,6 +1,6 @@
 export const colors = {
   bgPrimary: '#181818',
-  bgSecondary: '#2b2b2b',
+  bgSecondary: '#252525',
 
   textPrimary: '#fff',
   textSecondary: '#b2b2b2',
